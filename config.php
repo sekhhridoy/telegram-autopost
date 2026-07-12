e4aff384fbed8978c5a056bae20d6ca0314570b1
sekhhridoy <moviebd30@gmail.com>
2026-07-12 14:47:56 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,1 @@
-<?php
 
-return [
-    // Telegram Bot Token
-    'bot_token' => '8518044652:AAFn9FRIgOoHs_WPwVCZwmD7mhitfrZdV5I',
-
-    // Telegram Channel ID
-    'chat_id' => '-1002677113544',
-
-    // WordPress Movies API
-    'api_url' => 'https://filmi.unaux.com/wp-json/wp/v2/movies?per_page=1',
-
-    // Website URL
-    'website' => 'https://filmi.unaux.com',
-
-    // Storage file
-    'storage' => __DIR__ . '/storage.json',
-];
